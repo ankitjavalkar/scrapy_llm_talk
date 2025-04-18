a8e504e2c33fc67daec771cdb07d7fb021ca9422
ankitjavalkar <ankitjavalkar@users.noreply.github.com>
2025-04-18 14:33:22 +0530

    Add a basic spider script skeleton

--- llm_spider_project/spiders/llm_spider.py
+++ llm_spider_project/spiders/llm_spider.py
@@ -9,3 +9,19 @@ from scrapy import Spider
 
 html_cleaner = HTML2Text()
 logger = getLogger(__name__)
+
+
+class BooksToScrapeComLLMSpider(Spider):
+    name = "books_toscrape_com_llm"
+    start_urls = [
+        "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
+    ]
+
+    def parse(self, response):
+        # Parse the book links on the listing page
+        # Parse the pagination link and parse the next page
+
+    async def parse_book(self, response):
+        # Define the field names and field data prompts
+        # Call the LLM via API
+        # Result data
